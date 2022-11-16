2cd6f64a3d916ed1a05bc2716d43b075ff12328c
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-11-16 16:14:37 +0300

    add fixture for open/close ChromeDriver

---
+++ conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(scope='function') # по умолчанию всегда fanction
+def driver():
+    driver = webdriver.Chrome(ChromeDriverManager().install()) # если браузер установлен, то будет браться из кеша
+    driver.maximize_window()
+    yield
+    driver.quit()
+
+
+
+
+
