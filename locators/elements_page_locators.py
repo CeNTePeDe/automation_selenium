461d0fc1cd9db6fe42d3dcedba4faef7f98a48f3
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-11-21 16:42:19 +0300

    add class CheckPageLocators

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -13,3 +13,9 @@ class TextBoxPageLocators:
     CREATED_EMAIL =(By.XPATH, "//p[@id='email'] ")
     CREATED_CURRENT_ADDRESS =(By.XPATH, "//p[@id='currentAddress']")
     CREATED_PERMANENT_ADDRESS =(By.XPATH, "//p[@id='permanentAddress']")
+
+
+class CheckBoxPageLocators:
+    EXPEND_ALL_BUTTON = (By.CSS_SELECTOR, "button[title='Expand all']")
+    ITEM_LIST = (By.CSS_SELECTOR, "span[class='rct-title']")
+    CHACEKED_ITEMS = (By.CSS_SELECTOR, "svg[class='rct-icon rct-icon-check']")
