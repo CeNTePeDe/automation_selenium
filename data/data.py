3bb8aa65e2a21cb647a6f290f89ea7a7cc3ad4fb
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-11-23 15:58:24 +0300

    add data first_name, last_name, age, salary, department

--- data/data.py
+++ data/data.py
@@ -4,6 +4,11 @@ from dataclasses import dataclass
 @dataclass
 class Person:
     FULL_NAME: str = None
+    FIRST_NAME: str = None
+    LAST_NAME: str = None
+    AGE: int = None
+    SALARY: int = None
+    DEPARTMENT: str = None
     EMAIL: str = None
     CURRENT_ADDRESS: str = None
     PERMANENT_ADDRESS: str = None
