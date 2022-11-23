d495485d71a27404cc33a8f9b38d23659f58150a
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-11-23 15:59:35 +0300

    add data

--- generator/generator.py
+++ generator/generator.py
@@ -1,3 +1,5 @@
+import random
+
 from data.data import Person
 from faker import Faker
 
@@ -8,6 +10,11 @@ Faker.seed()  # прописание работы faker на русском
 def generated_person():
     yield Person(
         FULL_NAME=faker_ru.first_name() + " " + faker_ru.last_name() + " " + faker_ru.middle_name(),
+        FIRST_NAME=faker_ru.first_name(),
+        LAST_NAME=faker_ru.last_name(),
+        AGE=random.randint(10, 90),
+        SALARY=random.randint(100, 10000),
+        DEPARTMENT=faker_ru.job(),
         EMAIL=faker_ru.email(),
         CURRENT_ADDRESS=faker_ru.address(),
         PERMANENT_ADDRESS=faker_ru.address()
