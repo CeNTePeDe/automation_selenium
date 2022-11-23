6978cca06755e52ccde51a18dd61024b8b71ba5a
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-11-23 16:25:54 +0300

    add func test_web_table_search_person

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,3 +1,4 @@
+import random
 import time
 from pages.element_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage
 
@@ -44,13 +45,24 @@ class TestElements:
     class TestWebTable:
 
         def test_web_table_add_person(self, driver):
-            web_person_page =  WebTablePage(driver, 'https://demoqa.com/webtables')
-            web_person_page.open()
-            new_person = web_person_page.add_new_person()
-            table_result = web_person_page.check_new_added_person()
-            print(new_person)
-            print(table_result)
+            web_table_page =  WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            new_person = web_table_page.add_new_person()
+            table_result = web_table_page.check_new_added_person()
             assert new_person in table_result
+
+        def test_web_table_search_person(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            key_word = web_table_page.add_new_person()[random.randint(0,5)]
+            web_table_page.search_some_person(key_word)
+            table_result = web_table_page.check_seach_person()
+            print(key_word)
+            print(table_result)
+            assert key_word in table_result,  "the person was not found in the table"
+
+
+
 
 
 
