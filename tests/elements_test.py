9689b59fdbbfd2f13bbc5894c14e8e85ecf906ce
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-11-24 15:37:19 +0300

    add class TestButtonPage

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,6 +1,6 @@
 import random
 import time
-from pages.element_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage
+from pages.element_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonsPage
 
 
 class TestElements:
@@ -85,3 +85,18 @@ class TestElements:
             count = web_table_page.select_up_to_some_rows()
             assert count == [5, 10, 20, 25, 50, 100], "The numbers of rows in the table has not " \
                                                       "been changed or has chenged incorrectly"
+
+    class TestButtonPage:
+
+        def test_different_click_on_the_buttons(self, driver):
+            button_page = ButtonsPage(driver, 'https://demoqa.com/buttons')
+            button_page.open()
+            double = button_page.click_on_different_button('double')
+            right = button_page.click_on_different_button('right')
+            click = button_page.click_on_different_button('click')
+            print(double)
+            print(right)
+            print(click)
+            assert double== "You have done a double click"
+            assert right == "You have done a right click"
+            assert click == "You have done a dynamic click"
