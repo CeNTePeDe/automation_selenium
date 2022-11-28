3ca7b74a3ad733194541255ef13429f2e188bf41
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-11-28 11:36:29 +0300

    add class UploadDownloadPage

--- pages/element_page.py
+++ pages/element_page.py
@@ -1,11 +1,13 @@
+import base64
+import os
 import random
 
 import requests
 from selenium.webdriver.common.by import By
 
-from generator.generator import generated_person
+from generator.generator import generated_person, generated_file
 from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators, RadioButtonPageLocators, \
-    WebTablesPageLocators, ButtonsPageLocators, LinkPageLocators
+    WebTablesPageLocators, ButtonsPageLocators, LinkPageLocators, UploadDownloadPageLocators
 from pages.base_page import BasePage
 
 
@@ -197,3 +199,28 @@ class LinksPage(BasePage):
             self.element_is_present(self.locators.BAD_REQUEST).click()
         else:
             return request.status_code
+
+class UploadDownloadPage(BasePage):
+    locators = UploadDownloadPageLocators()
+
+    def upload_file(self):
+        file_name, path = generated_file()
+        self.element_is_present(self.locators.UPLOAD_FILE).send_keys(path)
+        os.remove(path)
+        text = self.element_is_present(self.locators.UPLOADED_FILE).text
+        return file_name.split('\\')[-1], text.split('\\')[-1]
+
+    def download_file(self):
+        link = self.element_is_present(self.locators.DOWNLOAD_FILE).get_attribute('href')
+        link_b = base64.b64decode(link)
+        path_name = rf'C:\Users\Anton\PycharmProjects\automation_selenium\filetest{random.randint(0,999)}.jpg'
+        with open(path_name, 'wb+') as f:
+            offset = link_b.find(b'\xff\xd8')
+            f.write(link_b[offset:])
+            check_file = os.path.exists(path_name)
+            f.close()
+        os.remove(path_name)
+        return check_file
+
+
+
