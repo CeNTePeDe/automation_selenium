c97a24890f5eae1c363104c01fbe647441576964
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-12-06 15:37:48 +0300

    class TestAlertPage

--- tests/alerts_frame_windows_test.py
+++ tests/alerts_frame_windows_test.py
@@ -1,4 +1,7 @@
-from pages.alerts_fram_win_page import BrowserWindowsPage
+import time
+
+from pages.alerts_fram_win_page import BrowserWindowsPage, AlertsPage
+
 
 class TestAlertsFrameWindow:
 
@@ -16,5 +19,34 @@ class TestAlertsFrameWindow:
             new_window.open()
             text_result = new_window.check_opened_new_window()
             assert text_result == 'This is a sample page'
+
+    class TestAlertPage:
+
+        def test_see_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            text = alert_page.check_see_alert()
+            assert text == 'You clicked a button'
+
+        def test_alert_appear_5_sec(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            text = alert_page.check_alert_appear_5_sec()
+            assert text == 'This alert appeared after 5 seconds'
+
+        def test_confirm_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            text =alert_page.check_confirm_alert()
+            assert text == 'You selected Ok'
+
+        def test_alert_promt_result(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            text, text_result = alert_page.check_alert_promt_result()
+            assert text_result == f'You entered {text}'
+            assert text in text_result
+
+
 
 
