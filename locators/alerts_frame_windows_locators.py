bf27b4ff4c7cca980ccaa738ba7ec9b978f3b68b
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-12-06 15:37:37 +0300

    class AlertsPageLocators

--- locators/alerts_frame_windows_locators.py
+++ locators/alerts_frame_windows_locators.py
@@ -7,3 +7,12 @@ class BrowserWindowsPageLocators:
     NEW_WINDOW_MESSAGE = (By.CSS_SELECTOR, 'button[id="messageWindowButton"]')
 
     TEXT_PAGE = (By.CSS_SELECTOR, 'h1[id = "sampleHeading"]')
+
+class AlertsPageLocators:
+    ALERT_BUTTON = (By.CSS_SELECTOR, 'button[id="alertButton"]')
+    TIME_ALERT_BUTTON = (By.CSS_SELECTOR, 'button[id="timerAlertButton"]')
+    CONFIRM_BUTTON = (By.CSS_SELECTOR, 'button[id="confirmButton"]')
+    PROMT_BUTTON = (By.CSS_SELECTOR, 'button[id="promtButton"]')
+
+    CONFIRM_RESULT = (By.CSS_SELECTOR, 'span[id="confirmResult"]')
+    PROMT_RESULT = (By.CSS_SELECTOR, 'span[id = "promptResult"]')
