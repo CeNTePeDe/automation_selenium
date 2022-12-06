7cd8bfb85ef2159eae6cb916acdbdf4fb49acd75
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-12-06 15:37:29 +0300

    class AlertsPage

--- pages/alerts_fram_win_page.py
+++ pages/alerts_fram_win_page.py
@@ -1,4 +1,7 @@
-from locators.alerts_frame_windows_locators import BrowserWindowsPageLocators
+import random
+import time
+
+from locators.alerts_frame_windows_locators import BrowserWindowsPageLocators, AlertsPageLocators
 from pages.base_page import BasePage
 
 
@@ -17,3 +20,34 @@ class BrowserWindowsPage(BasePage):
         self.driver.switch_to.window(self.driver.window_handles[1])
         text_title = self.element_is_present(self.locator.TEXT_PAGE).text
         return text_title, 'the new tab has not opened or an incorrecttab has opened'
+
+class AlertsPage(BasePage):
+
+    locator = AlertsPageLocators()
+
+    def check_see_alert(self):
+        self.element_is_visible(self.locator.ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        return alert_window.text
+
+    def check_alert_appear_5_sec(self):
+        self.element_is_visible(self.locator.TIME_ALERT_BUTTON).click()
+        time.sleep(5)
+        alert_window = self.driver.switch_to.alert
+        return alert_window.text
+
+    def check_confirm_alert(self):
+        self.element_is_visible(self.locator.CONFIRM_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        alert_window.accept()
+        text_result = self.element_is_present(self.locator.CONFIRM_RESULT).text
+        return text_result
+
+    def check_alert_promt_result(self):
+        text = f'hello, world {random.randint(1,98)}'
+        self.element_is_visible(self.locator.PROMT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        alert_window.send_keys(text)
+        alert_window.accept()
+        text_result = self.element_is_visible(self.locator.PROMT_RESULT).text
+        return text, text_result
