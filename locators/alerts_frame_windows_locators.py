7fc09b977c48a09e4201a9ba3f4395d216f6f254
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-12-06 16:14:25 +0300

    added class FramePageLocator

--- locators/alerts_frame_windows_locators.py
+++ locators/alerts_frame_windows_locators.py
@@ -8,6 +8,7 @@ class BrowserWindowsPageLocators:
 
     TEXT_PAGE = (By.CSS_SELECTOR, 'h1[id = "sampleHeading"]')
 
+
 class AlertsPageLocators:
     ALERT_BUTTON = (By.CSS_SELECTOR, 'button[id="alertButton"]')
     TIME_ALERT_BUTTON = (By.CSS_SELECTOR, 'button[id="timerAlertButton"]')
@@ -16,3 +17,10 @@ class AlertsPageLocators:
 
     CONFIRM_RESULT = (By.CSS_SELECTOR, 'span[id="confirmResult"]')
     PROMT_RESULT = (By.CSS_SELECTOR, 'span[id = "promptResult"]')
+
+
+class FramePageLocator:
+    FIRST_FRAME = (By.CSS_SELECTOR, 'iframe[id="frame1"]')
+    SECOND_FRAME = (By.CSS_SELECTOR, 'iframe[id="frame2"]')
+
+    TITLE_FRAME = (By.CSS_SELECTOR, 'h1[id = "sampleHeading"]')
