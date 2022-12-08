39bae96bed2c5dcbe4507dbbda5850815bc67af4
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-12-08 18:24:48 +0300

    added class AutoCompletePageocators

--- locators/widgets_locators.py
+++ locators/widgets_locators.py
@@ -9,3 +9,11 @@ class AccordianPageLocators:
     SECTION_1_CONTENT = (By.CSS_SELECTOR, 'div[id="section1Content"] p')
     SECTION_2_CONTENT = (By.CSS_SELECTOR, 'div[id="section2Content"] p')
     SECTION_3_CONTENT = (By.CSS_SELECTOR, 'div[id="section3Content"] p')
+
+class AutoCompletePageocators:
+    MULTI_COLOR_NAMES =(By.CSS_SELECTOR, 'input[id="autoCompleteMultipleInput"]')
+    MULTI_VALUE = (By.CSS_SELECTOR, 'div[class="css-1rhbuit-multiValue auto-complete__multi-value"]')
+    MULTI_VALUE_REMOVE = (By.CSS_SELECTOR, 'div[class="css-1rhbuit-multiValue auto-complete__multi-value"] svg path')
+
+    SINGLE_VALUE = (By.CSS_SELECTOR, 'div[class="auto-complete__single-value css-1uccc91-singleValue"]')
+    SINGLE_COLOR_NAME = (By.CSS_SELECTOR, 'input[id="autoCompleteSingleInput"]')
