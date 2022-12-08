de7984dcff7664e94f8250efd5284017f7cd40a3
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-12-08 14:46:14 +0300

    added class TestModalDialog

--- tests/alerts_frame_windows_test.py
+++ tests/alerts_frame_windows_test.py
@@ -1,10 +1,9 @@
 import time
 
-from pages.alerts_fram_win_page import BrowserWindowsPage, AlertsPage, FramePage, NestedFramePage
+from pages.alerts_fram_win_page import BrowserWindowsPage, AlertsPage, FramePage, NestedFramePage, ModalDialogPage
 
 
 class TestAlertsFrameWindow:
-
     class TestBrowserWindows:
 
         def test_new_tab(self, driver):
@@ -12,7 +11,6 @@ class TestAlertsFrameWindow:
             new_tab_page.open()
             text_result = new_tab_page.check_open_new_tab()
             assert text_result == 'This is a sample page'
-
 
         def test_new_window(self, driver):
             new_window = BrowserWindowsPage(driver, 'https://demoqa.com/browser-windows')
@@ -37,7 +35,7 @@ class TestAlertsFrameWindow:
         def test_confirm_alert(self, driver):
             alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
             alert_page.open()
-            text =alert_page.check_confirm_alert()
+            text = alert_page.check_confirm_alert()
             assert text == 'You selected Ok'
 
         def test_alert_promt_result(self, driver):
@@ -48,13 +46,13 @@ class TestAlertsFrameWindow:
             assert text in text_result
 
     class TestFramePage:
-         def test_frames(self, driver):
-             frame_page = FramePage(driver, 'https://demoqa.com/frames')
-             frame_page.open()
-             result1 =  frame_page.check_frame('frame1')
-             result2 =  frame_page.check_frame('frame2')
-             assert result1 == ['This is a sample page', '500px', '350px'], 'The frame does not exist'
-             assert result2 == ['This is a sample page', '100px', '100px'], 'The frame does not exist'
+        def test_frames(self, driver):
+            frame_page = FramePage(driver, 'https://demoqa.com/frames')
+            frame_page.open()
+            result1 = frame_page.check_frame('frame1')
+            result2 = frame_page.check_frame('frame2')
+            assert result1 == ['This is a sample page', '500px', '350px'], 'The frame does not exist'
+            assert result2 == ['This is a sample page', '100px', '100px'], 'The frame does not exist'
 
     class TestNestedFramePage:
         def test_nested_frame(self, driver):
@@ -64,7 +62,12 @@ class TestAlertsFrameWindow:
             assert parent_text == 'Parent frame', 'Nested frame does not exist'
             assert child_text == 'Child Iframe', 'Nested frame does not exist'
 
+    class TestModalDialog:
+        def test_modal_dialog(self, driver):
+            modal_dialog_page = ModalDialogPage(driver, 'https://demoqa.com/modal-dialogs')
+            modal_dialog_page.open()
+            text_small_modal_dialog, text_large_modal_dialog = modal_dialog_page.check_modal_dialog_button()
 
-
-
-
+            assert text_large_modal_dialog[
+                   :74] == "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            assert text_small_modal_dialog == "This is a small modal. It has very less content"
