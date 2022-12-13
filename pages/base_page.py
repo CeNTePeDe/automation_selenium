b68c75d71747974b90dab011388f5a9f2a07a24c
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-12-13 15:33:32 +0300

    added func action_drag_and_drop_to_element

--- pages/base_page.py
+++ pages/base_page.py
@@ -60,4 +60,8 @@ class BasePage:
         action.drag_and_drop_by_offset(element, x_coords, y_coords)
         action.perform()
 
+    def action_drag_and_drop_to_element(self,what, where): # какой элемент и куда перенести
+        action = ActionChains(self.driver)
+        action.drag_and_drop(what, where)
+        action.perform()
 
