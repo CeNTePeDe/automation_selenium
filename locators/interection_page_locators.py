41786dc4b7b2bf8f0f39ba6a799318f2b639344d
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2022-12-15 16:00:14 +0300

    added class DropablePageLocators

--- locators/interection_page_locators.py
+++ locators/interection_page_locators.py
@@ -24,3 +24,30 @@ class ResizeblePageLocators:
          'div[id= "resizable"] [class="react-resizable-handle react-resizable-handle-se"]')
     HANDLE = (By.CSS_SELECTOR, 'div[id= "resizable"] ')
 
+class DropablePageLocators:
+    #TABS:
+    SIMPLE_TABS = (By.CSS_SELECTOR, 'a[id="droppableExample-tab-simple"]')
+    ACCEPT_TABS = (By.CSS_SELECTOR, 'a[id="droppableExample-tab-accept"]')
+    PREVENT_TABS = (By.CSS_SELECTOR, 'a[id="droppableExample-tab-preventPropogation"]')
+    REVERT_DRAGGABLE_TAB = (By.CSS_SELECTOR, 'a[id="droppableExample-tab-revertable"]')
+
+    #simple tabs
+    DRAG_ME = (By.CSS_SELECTOR, 'div[id="draggable"]')
+    DROPED = (By.CSS_SELECTOR, '#simpleDropContainer #droppable')
+
+    #accept tabs
+    ACCEPTABLE = (By.CSS_SELECTOR, 'div[id="acceptable"]')
+    NOT_ACCEPTABLE = (By.CSS_SELECTOR, 'div[id="notAcceptable"]')
+    DROP_HER = (By.CSS_SELECTOR, '#acceptDropContainer #droppable')
+
+    #prevent propogation
+    NOT_GREEDY_DROP_BOX_TEXT = (By.CSS_SELECTOR, 'div[id="notGreedyDropBox"] p:nth-child(1)')
+    NOT_GREEDY_INNER_BOX = (By.CSS_SELECTOR, 'div[id="notGreedyInnerDropBox"]')
+    GREEDY_DROP_BOX_TEXT = (By.CSS_SELECTOR, 'div[id="greedyDropBox"] p:nth-child(1)')
+    GREEDY_INNER_BOX = (By.CSS_SELECTOR, 'div[id="greedyDropBoxInner"]')
+    DRAG_ME_PREVENT = (By.CSS_SELECTOR, '#ppDropContainer #dragBox')
+
+    #revert dragable
+    WILL_REVERT = (By.CSS_SELECTOR, 'div[id="revertable"]')
+    NOT_REVERT = (By.CSS_SELECTOR, 'div[id="notRevertable"]')
+    DROP_HERE_REVERT = (By.CSS_SELECTOR, '#revertableDropContainer #droppable')
