23c88ba0bedac2c8fb950a5402adfb9990cf86c2
Olga Orlova <108580276+CeNTePeDe@users.noreply.github.com>
2023-01-10 12:49:41 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-@pytest.fixture(scope='function') # по умолчанию всегда fanction
+@pytest.fixture(scope='function') # по умолчанию всегда function
 def driver():
     driver = webdriver.Chrome(ChromeDriverManager().install()) # если браузер установлен, то будет браться из кеша
     driver.maximize_window()
